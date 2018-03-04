723542beeff67d6c12bfc0ea5c865fd52e59bc9a
teddy ed <gyx_contact@163.com>
2018-03-04 23:42:37 +0800

    first commit

---
+++ Assets/Editor/ProtoBufTest.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using gameplay;
+using System.IO;
+
+public class ProtoBufTest {
+
+	[Test]
+	public void ProtoBufTestSimplePasses() {
+        // Use the Assert class to test conditions.
+        //Arrange
+        HeartBeat inbeat = new HeartBeat();
+        inbeat.TimeStamp = 1.989f;
+
+        //Act
+        var ser = proto.ProtobufSerializer.Create();
+        MemoryStream stream = new MemoryStream();
+        ser.Serialize(stream, inbeat);
+
+        stream.Seek(0, SeekOrigin.Begin);
+
+        HeartBeat outbeat = null;
+        outbeat = ser.Deserialize(stream, outbeat, typeof(HeartBeat)) as HeartBeat;
+
+        //Assert
+        Assert.AreEqual(inbeat.TimeStamp, outbeat.TimeStamp);
+    }
+
+    /*
+	// A UnityTest behaves like a coroutine in PlayMode
+	// and allows you to yield null to skip a frame in EditMode
+	[UnityTest]
+	public IEnumerator ProtoBufTestWithEnumeratorPasses() {
+		// Use the Assert class to test conditions.
+		// yield to skip a frame
+
+		yield return null;
+	}*/
+}
---
+++ Assets/Scripts/Logic/Controller/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Nexus.Logic.Controller
+{
+    public class PlayerController : NetworkBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (isLocalPlayer)
+            {
+                var x = Input.GetAxis("Horizontal") * Time.deltaTime * 50.0f;
+                var z = Input.GetAxis("Vertical") * Time.deltaTime * 50.0f;
+
+                //transform.Rotate(0, x, 0);
+                transform.Translate(x, 0, z);
+            }
+        }
+
+        public override void OnStartLocalPlayer()
+        {
+            GetComponentInChildren< SpriteRenderer>().material.color = Color.blue;
+        }
+    }
+}
